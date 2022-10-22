67ffe11ea4211343f8bf322a6fb579505bbb6e95
Michele Alberti <62114934+Michele-Alberti@users.noreply.github.com>
2022-10-22 17:43:41 +0200

    release 1.7.3 (previous release 1.7.2)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="data_lunch_cli",
-    version="1.7.2",
+    version="1.7.3",
     py_modules=["data_lunch_app"],
     install_requires=[
         "sqlalchemy==1.4.39",
