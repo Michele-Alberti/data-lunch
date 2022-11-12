d1b822dd2b3340cfae2974d3f1e7c33fbff8b1e2
Michele Alberti <62114934+Michele-Alberti@users.noreply.github.com>
2022-11-12 15:03:01 +0100

    feat(cli.py): add commands to command line interface
    add commands for cleaning database and dropping tables

--- data_lunch_app/cli.py
+++ data_lunch_app/cli.py
@@ -7,7 +7,10 @@ from .models import create_database, create_engine
 
 # Import database object
 from .models import db as sql_alchemy_db
-from .models import Menu, Orders
+from .models import Menu, Orders, Users, Stats
+
+# Import functions from core
+from .core import clean_tables as clean_tables_func
 
 # Version
 __version__ = pkg_resources.require("data_lunch_cli")[0].version
@@ -61,7 +64,46 @@ def delete_database(obj):
         click.secho("Database deleted", fg="green")
     except Exception:
         # Generic error
-        click.secho("Cannot delete database for unknown reason", fg="red")
+        click.secho("Cannot delete database for unknown reasons", fg="red")
+
+
+@db.command("clean")
+@click.confirmation_option()
+@click.pass_obj
+def clean_tables(obj):
+    """Clean 'users', 'menu' and 'orders' tables."""
+
+    # Drop table
+    try:
+        clean_tables_func(obj["config"])
+        click.secho("done", fg="green")
+    except Exception:
+        # Generic error
+        click.secho("Cannot clean database for unknown reasons", fg="red")
+
+
+@db.group()
+@click.pass_obj
+def table(obj):
+    """Manage tables in database."""
+
+
+@table.command("drop")
+@click.confirmation_option()
+@click.argument("name")
+@click.pass_obj
+def delete_table(obj, name):
+    """Drop a single table from database."""
+
+    # Drop table
+    try:
+        engine = create_engine(obj["config"])
+        command = f"DROP TABLE {name};"
+        engine.execute(command)
+        click.secho(f"Table '{name}' deleted", fg="green")
+    except Exception:
+        # Generic error
+        click.secho("Cannot delete database for unknown reasons", fg="red")
 
 
 def main():
